63a035bb57701ba321736c4c769680ff75a27b8a
tomasgvgt <tomasgvg.t@gmail.com>
2022-10-22 09:54:39 -0500

    Delete mongoose.Promise = global.Promise It is not necessary since mongoose 5

--- components/message/storage.js
+++ components/message/storage.js
@@ -1,26 +1,33 @@
-const db = require('mongoose');
+const mongoose = require('mongoose');
 const Model = require('./model');
 const {DB_HOST, DB_USER, DB_PASS} = require('../../credentials')
 const uri = `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}?retryWrites=true&w=majority`;
 
-
-
-db.Promise = global.Promise;
-
-db.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(()=> console.log('[db] Succesfully connected'))
     .catch((err)=>console.error(`[db] ${err}`));
 
-function saveMessage(fullMessage){
-    const newMessage = new Model(fullMessage);
-    newMessage.save();
-    console.log('Message succesfully Stored');
-}
 
 async function retrieveMessages(){
-    const messages = await Model.find();
-    console.log('Messages retrieved')
-    return messages;
+    try{
+        const messages = await Model.find();
+        console.log('Messages retrieved')
+        return messages;
+    }catch(error){
+        console.error(error);
+        return false;
+    }
+}
+
+async function saveMessage(fullMessage){
+    try{
+        const newMessage = new Model(fullMessage);
+        await newMessage.save();
+        console.log('Message stored');
+        return true;
+    }catch(err){
+        return false;
+    }
 }
 
 module.exports = {
